236d725dc61b4bc6bdcb5183e915881f13aa86d2
Jacob Hildebrandt <jacob@commerce7.com>
2023-01-15 17:05:36 -0800

    Working Codex

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
-const configuration = new Configuration({ apikey: process.env.OPENAI_API_KEY });
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 
 const openai = new OpenAIApi(configuration);
 
